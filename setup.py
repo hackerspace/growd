dfaf19b88a7e2f604a6a17bc8ab9a37dec69bc07
Martin Milata <mmilata@srck.net>
2015-04-14 22:43:30 +0200

    Rudimentary growd-main

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Martin Milata',
     author_email='b42@srck.net',
     packages=find_packages(),
-    scripts=['bin/growd-test', 'bin/growd-tempsender'],
+    scripts=['bin/growd-test', 'bin/growd-tempsender', 'bin/growd-main'],
     install_requires=[],
     test_suite='nose.collector',
     tests_require=['nose'],
