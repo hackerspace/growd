6913acb0b2a2fbe1a04f69862b673bfb62819d09
Martin Milata <mmilata@srck.net>
2015-04-14 22:44:35 +0200

    relay: connect on low

--- growd/hw/relay.py
+++ growd/hw/relay.py
@@ -1,10 +1,15 @@
 class Relay(object):
-    def __init__(self, device, pin, initially_on=False):
+    def __init__(self, device, pin, initially_on=False, connect_on_low=False):
         self.device = device
         self.pin = pin
+        self.connect_on_low = connect_on_low
 
         self.device.gpio_setup(pin, True)
         self.switch(initially_on)
 
     def switch(self, switch_on):
-        self.device.gpio_set(self.pin, switch_on)
+        state = switch_on
+        if self.connect_on_low:
+            state = not state
+
+        self.device.gpio_set(self.pin, state)
