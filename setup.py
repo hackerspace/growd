f502350cec1f78e9bb885b4562279ff2b476b153
Martin Milata <mmilata@srck.net>
2016-03-27 22:16:16 +0200

    Drop growd-tempsender

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Martin Milata',
     author_email='b42@srck.net',
     packages=find_packages(),
-    scripts=['bin/growd-test', 'bin/growd-tempsender', 'bin/growd-main', 'bin/growd-ph'],
+    scripts=['bin/growd-test', 'bin/growd-main', 'bin/growd-ph'],
     install_requires=[],
     test_suite='nose.collector',
     tests_require=['nose'],
